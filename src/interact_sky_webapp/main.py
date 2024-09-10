fa7d4629b6611d4fef06939abda30ccf5d7d6bc9
orionlee <orionlee@yahoo.com>
2024-09-10 11:19:11 -0700

    minor optimization: avoid using fast cadence TPFs

--- src/interact_sky_webapp/main.py
+++ src/interact_sky_webapp/main.py
@@ -288,6 +288,10 @@ async def create_app_body_ui(tic, sector, magnitude_limit=None):
     aperture_mask = "pipeline"
     cutout_size = None
     sr = lk.search_targetpixelfile(f"TIC{tic}", mission="TESS", sector=sector)
+    if len(sr) > 1:
+        # exclude fast cadence data (20s), TPFs with fast cadence always has 2 min cadence counterparts
+        # for the use case here, the fast cadence data is irrelevant. It'd just make the processing slower.
+        sr = sr[sr.exptime > 60 *u.s]
     if len(sr) < 1:
         print(f"INFO: no TPF found for {msg_label}. Use TessCut.")
         aperture_mask = None  # N/A for TessCut
